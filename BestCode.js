f8ccde0587efa3a14a92006a84031472c833624d
Озорнин Ярослав <Runboll023@gmail.com>
2023-11-30 18:16:35 +0500

    first commit

---
+++ BestCode.js
@@ -0,0 +1,53 @@
+let mem = new Array();
+
+let fs = require('fs');
+inText = fs.readFileSync('program.splm')
+inText = inText.toString();
+mem = inText.split(/ |\r\n/);
+mem.push('exit');
+
+let ip =0;
+
+while(mem[ip]!='exit')
+    switch(mem[ip]){
+        case 'set':
+
+            mem[mem[ip+1]] = parseInt(mem[ip + 2]);
+            ip+=3;
+            break
+
+        case 'nok':
+
+            let max = Math.max(mem[mem[ip+1]], mem[mem[ip+2]]);
+            let lcm = max;
+            while (true) {
+                if (lcm % mem[mem[ip+1]] == 0 && lcm % mem[mem[ip+2]] == 0) {
+                    break;
+                }
+                lcm += max;
+            }
+            mem[mem[ip+3]] = lcm;
+            ip+=4;
+            break;
+
+        case 'fib':
+
+            function fib(n){
+                    if(n<=1){
+                        return n;
+                    }
+                    else{
+                        return fib(n - 1) + fib(n - 2);
+                    }
+                }
+                let n = mem[mem[ip + 1]];
+                mem[mem[ip+2]] = fib(n);
+                ip += 3;
+
+        case 'output1':
+
+            console.log(mem[mem[ip+1]])
+            ip += 2;
+            break;
+            
+}
